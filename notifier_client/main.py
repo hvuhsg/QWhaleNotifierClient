c19a59c6c6faa81689abb157ce26f6b0f63833f6
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-03-25 18:40:21 +0200

    Bug fix

--- notifier_client/main.py
+++ notifier_client/main.py
@@ -8,7 +8,9 @@ class NotifierClient:
     def __init__(self, app_token: str):
         self.app_token = app_token
 
-    def send_notification(self, user_token: str, title: str, body: str, url: str = "", data: dict = dict) -> dict:
+    def send_notification(self, user_token: str, title: str, body: str, url: str = "", data: dict = None) -> dict:
+        if data is None:
+            data = dict()
         data = dumps(data)
         response = requests.put(
             NOTIFIER_URL + f"/notifications/{user_token}?app_token={self.app_token}&title={title}&message={body}&url={url}",
